f4c0e325974deb674220df696eb12ae7bcc4be7e
Undef033 <undef033@gmail.com>
2022-07-16 06:25:34 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use rand::prelude::*;
-use std::io::Error;
 
 fn generate_pwd(len: u8, uppercase: bool, special_chars: bool) -> String {
     let mut chars = "abcdefghijklmnopqrstuvwxyz".to_string();
@@ -22,30 +21,35 @@ fn generate_pwd(len: u8, uppercase: bool, special_chars: bool) -> String {
     pwd
 }
 
-fn get_inp<T>(default: T) -> Result<T, Error>
-where
-    T: std::str::FromStr,
-{
+fn get_inp<T: std::str::FromStr>(default: T) -> Result<T, std::io::Error> {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line)?;
 
     Ok(line.trim().parse::<T>().unwrap_or(default))
 }
 
-fn main() -> Result<(), Error> {
+fn get_inp_b(default: bool) -> Result<bool, std::io::Error> {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line)?;
+
+    Ok(match line.trim().to_lowercase().as_str() {
+        "y" | "1" => true,
+        "n" | "0" => false,
+        _ => default,
+    })
+}
+
+fn main() -> Result<(), std::io::Error> {
     println!("[16] Password length: ");
     let length = get_inp::<u8>(16)?;
 
-    println!("[1] Special characters [1/0]: ");
-    let special_chars = get_inp::<i8>(1)?;
+    println!("[y] Special characters [Y/n]: ");
+    let special_chars = get_inp_b(true)?;
 
-    println!("[1] Uppercase [1/0]: ");
-    let uppercase = get_inp::<i8>(1)?;
+    println!("[y] Uppercase [Y/n]: ");
+    let uppercase = get_inp_b(true)?;
 
-    println!(
-        "{}",
-        generate_pwd(length, uppercase != 0, special_chars != 0)
-    );
+    println!("{}", generate_pwd(length, uppercase, special_chars));
 
     Ok(())
 }
