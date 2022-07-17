10e2a9decbe1a4c4318abbe2f487812bf15ce1ac
Undef033 <96497590+Undef033@users.noreply.github.com>
2022-07-17 06:06:12 +0300

    Delete ui.rs

--- pwd_gen/gui/src/ui.rs
+++
@@ -1,9 +0,0 @@
-impl eframe::epi::App for crate::generator::Generator {
-    fn update(&mut self, ctx: &eframe::egui::CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
-        self.update_ui(ctx);
-    }
-
-    fn name(&self) -> &str {
-        "Password Generator"
-    }
-}
